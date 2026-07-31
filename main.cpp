c73d3131972dbb4d7e90b42c0591801dcf39a309
paras-banshkar <banshkar3000@gmail.com>
2026-08-01 00:45:25 +0530

    created a model directory and added files

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
-
+using namespace std;
+#include "Model/RubiksCube3dArray.cpp"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    RubiksCube3dArray object3DArray;
+    object3DArray.print();
+  //  if (object3DArray.isSolved()) cout<<"solved"<<endl;
+  //  else cout<<"not solved"<<endl;
+  //  vector<RubiksCube::MOVE> movesToShuffle=object3DArray.randomShuffleCube(3);
+   // for(auto move: movesToShuffle) cout<<object3DArray.getMove(move)<<" ";
+   // cout<<"\n";
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    object3DArray.l();
+    object3DArray.print();
+    if (object3DArray.isSolved()) cout<<"solved"<<endl;
+    else cout<<"not solved"<<endl;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    object3DArray.lPrime();
+    object3DArray.print();
+    if (object3DArray.isSolved()) cout<<"solved"<<endl;
+    else cout<<"not solved"<<endl;
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
